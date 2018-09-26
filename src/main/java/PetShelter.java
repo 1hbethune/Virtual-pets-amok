160cd35af819dc5f739222f801c24374f1ac42b0
1hbethune <1hbethune@att.net>
2018-09-25 21:45:54 -0400

    commit

--- src/main/java/PetShelter.java
+++ src/main/java/PetShelter.java
@@ -1,9 +1,35 @@
+import java.util.Collection;
 
 public class PetShelter {
 
 	public void add(OrganicDog organicDog) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public Pets findPets(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void add(OrganicCat organicCat) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void add(RoboticDog roboticDog) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void add(RoboticCat roboticCat) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Collection<Pets> getAllPets() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/test/java/PetShelterTest.java
+++ src/test/java/PetShelterTest.java
@@ -1,4 +1,34 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
+
+import org.junit.Test;
 
 public class PetShelterTest {
+
+	PetShelter underTest = new PetShelter();
+	OrganicDog organicDog = new OrganicDog("", "", "");
+	OrganicCat organicCat = new OrganicCat("", "", "");
+	RoboticDog roboticDog = new RoboticDog("", "", "");
+	RoboticCat roboticCat = new RoboticCat("", "", "");
+
+	@Test
+	public void shouldBeAbleToAddRoboticDog() {
+		underTest.add(organicDog);
+		Pets foundOrganicDog = underTest.findPets("1");
+		assertThat(foundOrganicDog, is(organicDog));
+	}
+
+	@Test
+	public void shouldBeAbleToAddOrganicDogAndOrganicCatAndRoboticDogAndRoboticCat() {
+		underTest.add(organicDog);
+		underTest.add(organicCat);
+		underTest.add(roboticDog);
+		underTest.add(roboticCat);
+		Collection<Pets> addedPets = underTest.getAllPets();
+		assertThat(addedPets, containsInAnyOrder(organicDog, organicCat, roboticDog, roboticCat));
+	}
 
 }
